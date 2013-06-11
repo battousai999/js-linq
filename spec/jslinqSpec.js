393c0b8a1e2f2cfce8f29983d26aa4863ecc5f79
Kurtis Jones <battousai999@gmail.com>
2013-06-11 16:30:27 -0600

    Further work on Jasmine unit tests (up to 'firstOrDefault').

--- spec/jslinqSpec.js
+++ spec/jslinqSpec.js
@@ -1,5 +1,36 @@
 describe('jslinq', function ()
 {
+    beforeEach(function ()
+    {
+        this.addMatchers({
+            toEqualIgnoringOrder: function (arr)
+            {
+                var result = this.actual;
+
+                if (arr == null || result == null || arr.length != result.length)
+                    return false;
+
+                var length = arr.length;
+
+                for (var i = 0; i < length; i++)
+                {
+                    var found = false;
+
+                    for (var j = 0; j < length; j++)
+                    {
+                        if (arr[i] == result[j])
+                            found = true;
+                    }
+
+                    if (!found)
+                        return false;
+                }
+
+                return true;
+            }
+        });
+    });
+
     describe('from (constructor)', function ()
     {
         var arr = [1, 2, 3, 4, 5, 6];
@@ -121,32 +152,298 @@ describe('jslinq', function ()
     describe('lambda function construction', function ()
     {
         var col = $linq([1, 2, 3, 4, 5]);
-        
-        var identity1 = col.select("x => x");
-        var identity2 = col.select("$");
-        var basic1 = col.select("x => x * 2");
-        var basic2 = col.select("$ * 2");
-        var comparer1 = col.orderByDescending("x => x", "x, y => x - y");
-        var comparer2 = col.orderByDescending("x => x", "(x, y) => x - y");
-        var comparer3 = col.orderByDescending("$", "$ - $$");
-        
+       
         it('handles identity functions', function ()
         {
+            var identity1 = col.select("x => x");
+            var identity2 = col.select("$");
+
             expect(identity1.toArray()).toEqual([1, 2, 3, 4, 5]);
             expect(identity2.toArray()).toEqual([1, 2, 3, 4, 5]);
         });
         
         it('handles basic functions', function ()
         {
+            var basic1 = col.select("x => x * 2");
+            var basic2 = col.select("$ * 2");
+
             expect(basic1.toArray()).toEqual([2, 4, 6, 8, 10]);
             expect(basic2.toArray()).toEqual([2, 4, 6, 8, 10]);
         });
         
         it('handles comparer functions', function ()
         {
+            var comparer1 = col.orderByDescending("x => x", "x, y => x - y");
+            var comparer2 = col.orderByDescending("x => x", "(x, y) => x - y");
+            var comparer3 = col.orderByDescending("$", "$ - $$");
+
             expect(comparer1.toArray()).toEqual([5, 4, 3, 2, 1]);
             expect(comparer2.toArray()).toEqual([5, 4, 3, 2, 1]);
             expect(comparer3.toArray()).toEqual([5, 4, 3, 2, 1]);
+        });
+    });
+
+    describe('where', function ()
+    {
+        var col = $linq([1, 2, 3, 4, 5]);
+
+        it('is correct with partitioned results', function () 
+        { 
+            var lessThan4 = col.where(function (x) { return x < 4; });
+
+            expect(lessThan4.toArray()).toEqual([1, 2, 3]); 
+        });
+
+        it('is correct with empty results', function () 
+        { 
+            var none = col.where(function (x) { return x < 0; });
+
+            expect(none.toArray()).toEqual([]); 
+        });
+
+        it('is correct with full results', function () 
+        { 
+            var all = col.where(function (x) { return x > 0; });
+
+            expect(all.toArray()).toEqual([1, 2, 3, 4, 5]); 
+        });
+
+        it('works with lambda functions', function () 
+        { 
+            var lambda = col.where("x => x < 4");
+
+            expect(lambda.toArray()).toEqual([1, 2, 3]); 
+        });
+
+        it('throws an exception on a null "predicate" parameter', function () { expect(function () { col.where(null); }).toThrow(); });
+    });
+
+    describe('select', function ()
+    {
+        var col = $linq([1, 2, 3, 4, 5]);
+
+        it('works by itself', function () 
+        { 
+            var doubleCol = col.select(function (x) { return x * 2; });
+
+            expect(doubleCol.toArray()).toEqual([2, 4, 6, 8, 10]); 
+        });
+
+        it('works in conjunction with "where"', function ()
+        {
+            var whereAndDouble = col.where(function (x) { return x < 4; }).select(function (x) { return x * 2; });
+            var whereAndDouble2 = col.where(function (x) { return x < 4; }).select(function (x) { return "a" + x; });
+
+            expect(whereAndDouble.toArray()).toEqual([2, 4, 6]);
+            expect(whereAndDouble2.toArray()).toEqual(["a1", "a2", "a3"]);
+        });
+
+        it('works with lambda functions', function ()
+        {
+            var lambda = col.select("x => x * 2");
+
+            expect(lambda.toArray()).toEqual([2, 4, 6, 8, 10]);
+        });
+
+        it('throws an exception on null "selector" parameter', function ()
+        {
+            expect(function () { col.select(null); }).toThrow();
+        });
+    });
+
+    describe('count', function ()
+    {
+        var col = $linq([1, 2, 3, 4, 5, 6, 7, 8]);
+
+        it('works without a predicate', function () { expect(col.count()).toEqual(8); });
+        it('works with a predicate', function () { expect(col.count(function (x) { return x % 2 == 0; })).toEqual(4); });
+        it('works with a lambda predicate', function () { expect(col.count('x => x % 2 == 0')).toEqual(4); });
+
+        it('throws an exception on non-function "predicate" parameter', function ()
+        {
+            expect(function () { col.count(99); }).toThrow();
+        });
+    });
+
+    describe('selectMany', function ()
+    {
+        var col = $linq([{ str: "test", list: [1, 2, 3] }, { str: "part", list: [4, 5, 6] }, { str: "time", list: [7, 8, 9] }]);
+
+        it('works with a projection', function ()
+        {
+            var projection = col.selectMany(function (x) { return x.list; }, function (x) { return "a" + x; });
+
+            expect(projection.toArray()).toEqualIgnoringOrder(["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9"]);
+        });
+
+        it('works with a projection and an index', function ()
+        {
+            var projectionAndIndex = col.selectMany(
+                function (x, i) { var l = x.list.slice(0); l.push((i + 1) * 10); return l; },
+                function (x) { return "b" + x; });
+
+            var projectionAndIndex2 = col.selectMany(
+                function (x, i) { var l = x.list.slice(0); l.push((i + 1) * 10); return l; },
+                function (x, parent) { return parent.str + "-" + "b" + x; });
+
+            expect(projectionAndIndex.toArray()).toEqualIgnoringOrder(["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10", "b20", "b30"]);
+            expect(projectionAndIndex2.toArray()).toEqualIgnoringOrder(["test-b1", "test-b2", "test-b3", "part-b4", "part-b5", "part-b6", "time-b7", "time-b8", "time-b9", "test-b10", "part-b20", "time-b30"]);
+        });
+
+        it('works with an embedded $linq within the projection', function ()
+        {
+            var linqInLinq = col.selectMany(
+                function (x) { return $linq(x.list).where(function (x) { return x % 2 == 0; }); },
+                function (x) { return "c" + x; });
+
+            expect(linqInLinq.toArray()).toEqualIgnoringOrder(["c2", "c4", "c6", "c8"]);
+        });
+
+        it('works with a lambda projection', function ()
+        {
+            var lambda = col.selectMany("x => x.list", "x => 'a' + x");
+
+            expect(lambda.toArray()).toEqualIgnoringOrder(["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9"]);
+        });
+
+        it('throws an exception on null "collection selector" parameter', function ()
+        {
+            expect(function () { col.selectMany(null); }).toThrow();
+        });
+
+        it('throws an exception on non-function "result selector" parameter', function ()
+        {
+            expect(function () { col.selectMany(function () { }, 99); }).toThrow();
+        });
+    });
+
+    describe('any', function ()
+    {
+        var col = $linq([2, 4, 6, 8]);
+
+        it('works without a predicate', function ()
+        {
+            var basicAny = col.any();
+            var basicNotAny = $linq([]).any();
+
+            expect(basicAny).toBeTruthy();
+            expect(basicNotAny).toBeFalsy();
+        });
+
+        it('works with a predicate', function ()
+        {
+            var anyEven = col.any(function (x) { return x % 2 == 0; });
+            var anyOdd = col.any(function (x) { return x % 2 == 1; });
+
+            expect(anyEven).toBeTruthy();
+            expect(anyOdd).toBeFalsy();
+        });
+
+        it('works with a lambda predicate', function ()
+        {
+            var lambda = col.any("x => x % 2 == 0");
+
+            expect(lambda).toBeTruthy();
+        });
+
+        it('throws an exception on non-function "predicate" parameter', function ()
+        {
+            expect(function () { col.any(99); }).toThrow();
+        });
+    });
+
+    describe('all', function ()
+    {
+        var allEvenCol = $linq([2, 4, 6, 8]);
+        var mixedCol = $linq([1, 2, 3, 4, 5, 6]);
+
+        it('works with a predicate', function ()
+        {
+            var allEven1 = allEvenCol.all(function (x) { return x % 2 == 0; });
+            var allEven2 = mixedCol.all(function (x) { return x % 2 == 0; });
+            var emptyCol = $linq([]).all(function (x) { return x % 2 == 0; });
+
+            expect(allEven1).toBeTruthy();
+            expect(allEven2).toBeFalsy();
+            expect(emptyCol).toBeTruthy();
+        });
+
+        it('works with a lambda predicate', function ()
+        {
+            var lambda = allEvenCol.all("x => x % 2 == 0");
+
+            expect(lambda).toBeTruthy();
+        });
+
+        it('throws an exception on null "predicate" parameter', function ()
+        {
+            expect(function () { mixedCol.all(null); }).toThrow();
+        });
+    });
+
+    describe('first', function ()
+    {
+        var col = $linq([1, 2, 3, 4, 5, 6]);
+
+        it('works with a predicate', function ()
+        {
+            var predicateFirst = col.first(function (x) { return x > 3; });
+
+            expect(predicateFirst).toEqual(4);
+        });
+
+        it('works without a predicate', function ()
+        {
+            var basicFirst = col.first();
+
+            expect(basicFirst).toEqual(1);
+        });
+
+        it('works with a lambda predicate', function ()
+        {
+            var lambda = col.first("x => x > 3");
+
+            expect(lambda).toEqual(4);
+        });
+
+        it('throws an exception on non-function "predicate" parameter', function ()
+        {
+            expect(function () { col.first(99); }).toThrow();
+        });
+    });
+
+    describe('firstOrDefault', function ()
+    {
+        var col = $linq([1, 2, 3, 4, 5, 6]);
+
+        it('works with a predicate', function ()
+        {
+            var defaultFirst1 = col.firstOrDefault(99, function (x) { return x > 3; });
+            var defaultFirst2 = col.firstOrDefault(99, function (x) { x > 100; });
+
+            expect(defaultFirst1).toEqual(4);
+            expect(defaultFirst2).toEqual(99);
+        });
+
+        it('works without a predicate', function ()
+        {
+            var defaultFirst1 = col.firstOrDefault(99);
+            var defaultFirst2 = $linq([]).firstOrDefault(99);
+
+            expect(defaultFirst1).toEqual(1);
+            expect(defaultFirst2).toEqual(99);
+        });
+
+        it('works with a lambda predicate', function ()
+        {
+            var lambda = col.firstOrDefault(99, "x => x > 3");
+
+            expect(lambda).toEqual(4);
+        });
+
+        it('throws an exception on non-function "predicate" parameter', function ()
+        {
+            expect(function () { col.firstOrDefault(99, 99); }).toThrow();
         });
     });
 });
