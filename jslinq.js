3376e1d09109ee4327228c06f0a78eb15795088a
Kurtis Jones <battousai999@gmail.com>
2016-08-04 16:56:04 -0600

    Changed version numbers in files.

--- jslinq.js
+++ jslinq.js
@@ -1,5 +1,5 @@
 /*
-    $linq Version 1.4.2 (by Kurtis Jones @ https://github.com/battousai999/js-linq)
+    $linq Version 1.5.0 (by Kurtis Jones @ https://github.com/battousai999/js-linq)
 */
 
 (function (window, undefined)
