08a0068c4ffe80494b51a0086514d5445c0e370b
Kurtis Jones <battousai999@gmail.com>
2018-02-16 16:08:33 -0700

    Add tests for toIterable and toArray

---
+++ tests/toArray-spec.js
@@ -0,0 +1,26 @@
+import {Linq} from '../jslinq';
+
+describe('toArray', () => 
+{
+    it('when called returns an array', () => 
+    {
+        let linq = new Linq([1, 2, 3, 4]);
+
+        expect(Linq.isArray(linq.toArray())).toBeTruthy();
+    });
+
+    it('when called on a Linq object that is empty returns an empty array', () => 
+    {
+        let linq = new Linq([]);
+
+        expect(linq.toArray()).toEqual([]);
+    });
+
+    it('when called on a Linq object that is not empty returns a non-empty array', () => 
+    {
+        let arr = [2, 4, 6, 8, 10];
+        let linq = new Linq(arr);
+
+        expect(linq.toArray()).toEqual(arr);
+    });
+});
